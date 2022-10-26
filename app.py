44d0b0a80968f5861394899f7836f098aed09750
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-10-26 18:48:01 +1100

    done

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import flask
 import urllib
+import os
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 @app.route('/<path:path>')
 def catch_all(path="/"):
-    print(flask.template_folder)
     netloc = urllib.parse.urlparse(flask.request.url).netloc
     if netloc in ("redirect.maxstuff.net", "localhost:5000"):
         return flask.render_template('index.html')
-    return flask.render_template('catch_all.html', path=flask.request.url, netloc=netloc), 410
+    if netloc+".html" in os.listdir(app.template_folder):
+        return flask.render_template(netloc+".html", path=flask.request.url, netloc=netloc), 410
+    return flask.render_template('wb_machine.html', path=flask.request.url, netloc=netloc), 410
